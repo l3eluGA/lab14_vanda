f7a59d3a66392325d0bc6e76c867b60f72a3e821
l3eluGA <rungthiporange@gmail.com>
2023-01-13 16:07:01 +0700

    lab14_1_complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,20 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[])
+{
+    double sum,sum2,sum3,multi=1,max=A[0],min=A[0];
+    int i=0;
+    for(i=0;i<N;i++) sum+=A[i];
+    B[0]=sum/N;
+    for(i=0;i<N;i++) sum2+=pow(A[i]-B[0],2);
+    B[1]=pow((sum2/(N)),0.5);
+    for(i=0;i<N;i++) multi*=A[i];
+    B[2]=pow(multi,1.0/N);
+    for(i=0;i<N;i++) sum3+=pow(A[i],-1);
+    B[3]=N/sum3;
+    for(i=0;i<N;i++) if(A[i]>max) max=A[i];
+    B[4]=max;
+    for(i=0;i<N;i++) if(A[i]<min) min=A[i];
+    B[5]=min;
+}
